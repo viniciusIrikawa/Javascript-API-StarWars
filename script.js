94cb4391ac070488ac11a85c48fe159bf58a7f83
viniciusIrikawa <viniciusiandrade@hotmail.com>
2021-07-31 21:59:26 -0300

    Basic changes

--- script.js
+++ script.js
@@ -1,28 +1,14 @@
-// fetch('https://swapi.dev/api/people/1')
-//     .then(function(response){
-//         return response.json();
-//     })
-//     .then(function(dados){
-//         console.log(dados);
-//         console.log('Nome do personagem: ' + dados.name);
-//         console.log('Veículo: ' + dados.vehicles[0]);
+const nomePerson = document.getElementById('name');
+const height = document.getElementById('height');
+const birthYear = document.getElementById('birth-year');
+const eyeColor = document.getElementById('eye-color');
 
-//     })
-
-const nome = document.getElementById('nome');
-const altura = document.getElementById('altura');
-const dataNasc = document.getElementById('data-nasc');
-const corOlhos = document.getElementById('cor-olhos');
-
-fetch('https://swapi.dev/api/people/83')
+fetch('https://swapi.dev/api/people/1')   
     .then((response) => response.json())
     .then((dados) => {
         console.log(dados);
-        // console.log(`Nome do personagem: ${dados.name}`);
-        // console.log(`Altura: ${dados.height} cm`);
-        // console.log(`Ano de nascimento: ${dados.birth_year} `);
-        nome.innerHTML = dados.name;
-        altura.innerHTML = dados.height + ' cm';
-        dataNasc.innerHTML = dados.birth_year;
-        corOlhos.innerHTML = dados.eye_color;
+        nomePerson.innerHTML = dados.name;
+        height.innerHTML = dados.height + ' cm';
+        birthYear.innerHTML = dados.birth_year;
+        eyeColor.innerHTML = dados.eye_color;
     });
